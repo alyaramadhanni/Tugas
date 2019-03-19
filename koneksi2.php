c09ac37ae1915e5a716fcd43d2ac6f9e96ef6e04
alyaramadhanni <48471940+alyaramadhanni@users.noreply.github.com>
2019-03-19 13:01:34 +0700

    Add files via upload

---
+++ koneksi2.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id8219133_tugas2","tugas2","id8219133_tugas2");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
